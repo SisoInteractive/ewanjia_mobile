348c4c1e0dd172c966126937ed561ef5400ee5db
aotuman <543484525@qq.com>
2015-01-17 21:03:37 +0800

    Remove js file , Remove type css file

--- js/common.js
+++ js/common.js
@@ -15,14 +15,6 @@
     }
     function autoSize(){
         var win_W = $(".container").width();
-        var win_poW=$(".pagination").width();
-        var win_po2W=$(".pagination2").width();
-        $(".pagination").css({
-            'margin-left':'-' + win_poW/2 + 'px'
-        });
-        $(".pagination2").css({
-            'margin-left':'-' + win_po2W/2 + 'px'
-        });
             var font = Math.round(win_W / 16);
             var HTML=document.getElementById('html');
             HTML.style.fontSize = font + 'px';
@@ -32,7 +24,6 @@
     addEvent(window,'load',autoSize);
     addEvent(window,'resize',autoSize);
     /*  end 添加用户窗口事件 */
-
 
 
 })()
